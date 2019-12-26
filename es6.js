240cdd974926d0fbd74adf6ed06c40d22f5938c7
nanut <galunyu@gmail.com>
2019-12-26 16:19:20 +0700

    first commit

---
+++ es6.js
@@ -0,0 +1,35 @@
+class EventObserver {
+  constructor() {
+    this.observers = []
+  }
+
+  subscribe(fn) {
+    this.observers.push(fn)
+    console.log(`You are now subscribed to ${fn.name}`)
+  }
+
+  unsubscribe(fn) {
+    this.observers = this.observers.filter((item) => {
+      if (item !== fn) return item
+    })
+    console.log(`You are now unsubscribe from ${fn.name}`)
+  }
+
+  fire() {
+    this.observers.forEach(item => item.call())
+  }
+}
+
+const click = new EventObserver()
+
+// @@ Event Listeners
+document.querySelector('.sub-ms').addEventListener(`click`, () => click.subscribe(getCurMilliseconds))
+
+document.querySelector('.unsub-ms').addEventListener(`click`, () => click.unsubscribe(getCurMilliseconds))
+
+document.querySelector('.fire').addEventListener(`click`, () => click.fire())
+
+// @@ Click Handler
+const getCurMilliseconds = function () {
+  console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`)
+}
